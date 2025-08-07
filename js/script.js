21f03fba3bbdb6c769b27c06f27cf1ff7a169856
Nonam1n <kiyotakanonami@email.com>
2025-08-07 22:56:34 +0500

    Almost done html and css but no js and json

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-"use strict"
-
-window.addEventListener('DOMContentLoaded', () => {
+"use strict";
+  window.addEventListener('DOMContentLoaded', () => {
     const burger = document.getElementById('burgerButton');
     const menu = document.getElementById('mobileMenu');
 
     burger.addEventListener('click', () => {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('open');
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('open');
     });
-
-});
+  });
