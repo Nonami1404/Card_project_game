9d843581df11a4e4c47694e40b886823f9d8c63d
Nonam1n <kiyotakanonami@email.com>
2025-08-07 22:15:26 +0500

    Cards random First commit

--- js/script.js
+++ js/script.js
@@ -9,4 +9,24 @@ window.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('open');
     });
 
+
+    //Cards choosing
+
+  const leftCard = document.querySelector("#left");
+  const rightCard = document.querySelector("#right");
+
+  const startFight = document.querySelector('.startFight');
+  startFight.addEventListener('click', () => {
+    const first = Math.floor(Math.random() * 20);
+    let second = first;
+
+    while(second === first){
+      second = Math.floor(Math.random() * 20);
+    }
+
+    console.log(first);//не обязательно
+    console.log(second);//не обязательно
+  })
+  //нужен массив с карточками чтобы оттуда взять две карты и затем добавить их в leftCard и rightCard
+
 });
